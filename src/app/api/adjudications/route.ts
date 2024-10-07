783c92f40d4a9966b9e3f6f3e9f45dc3c0e65ad6
charlesdoiron <doironcharles@gmail.com>
2024-10-07 20:20:03 +0200

    chore: Update maxDuration and revalidate values in adjudications route file

--- src/app/api/adjudications/route.ts
+++ src/app/api/adjudications/route.ts
@@ -19,8 +19,8 @@ import puppeteer from "puppeteer-core";
 
 import { NextResponse } from "next/server";
 import { scrapePropertyDetails } from "./utils/property";
-
-export const maxDuration = 60; // This function can run for a maximum of 60 seconds (update by 2024-05-10)
+export const revalidate = 0;
+export const maxDuration = 300;
 export const dynamic = "force-dynamic";
 
 export async function GET() {
@@ -47,59 +47,59 @@ export async function GET() {
       url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/outre-mer/historique-des-adjudications.html",
       region: "outre-mer",
     },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/outre-mer/prochaines-ventes.html",
-    //   region: "outre-mer",
-    // },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/outre-mer/prochaines-ventes.html",
+      region: "outre-mer",
+    },
 
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/centre-loire-limousin/historique-des-adjudications.html",
-    //   region: "centre-loire-limousin",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/centre-loire-limousin/prochaines-ventes.html",
-    //   region: "centre-loire-limousin",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/bretagne-grand-ouest/historique-des-adjudications.html",
-    //   region: "bretagne-grand-ouest",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/bretagne-grand-ouest/prochaines-ventes.html",
-    //   region: "bretagne-grand-ouest",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/regions-du-nord-est/historique-des-adjudications.html",
-    //   region: "regions-du-nord-est",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/regions-du-nord-est/prochaines-ventes.html",
-    //   region: "regions-du-nord-est",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-ouest-pyrenees/historique-des-adjudications.html",
-    //   region: "sud-ouest-pyrenees",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-ouest-pyrenees/prochaines-ventes.html",
-    //   region: "sud-ouest-pyrenees",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-est-mediterrannee/historique-des-adjudications.html",
-    //   region: "sud-est-mediterrannee",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-est-mediterrannee/prochaines-ventes.html",
-    //   region: "sud-est-mediterrannee",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/paris-et-ile-de-france/historique-des-adjudications.html",
-    //   region: "ile-de-france",
-    // },
-    // {
-    //   url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/paris-et-ile-de-france/prochaines-ventes.html",
-    //   region: "ile-de-france",
-    // },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/centre-loire-limousin/historique-des-adjudications.html",
+      region: "centre-loire-limousin",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/centre-loire-limousin/prochaines-ventes.html",
+      region: "centre-loire-limousin",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/bretagne-grand-ouest/historique-des-adjudications.html",
+      region: "bretagne-grand-ouest",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/bretagne-grand-ouest/prochaines-ventes.html",
+      region: "bretagne-grand-ouest",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/regions-du-nord-est/historique-des-adjudications.html",
+      region: "regions-du-nord-est",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/regions-du-nord-est/prochaines-ventes.html",
+      region: "regions-du-nord-est",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-ouest-pyrenees/historique-des-adjudications.html",
+      region: "sud-ouest-pyrenees",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-ouest-pyrenees/prochaines-ventes.html",
+      region: "sud-ouest-pyrenees",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-est-mediterrannee/historique-des-adjudications.html",
+      region: "sud-est-mediterrannee",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/sud-est-mediterrannee/prochaines-ventes.html",
+      region: "sud-est-mediterrannee",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/paris-et-ile-de-france/historique-des-adjudications.html",
+      region: "ile-de-france",
+    },
+    {
+      url: "https://www.licitor.com/ventes-aux-encheres-immobilieres/paris-et-ile-de-france/prochaines-ventes.html",
+      region: "ile-de-france",
+    },
   ];
 
   try {
